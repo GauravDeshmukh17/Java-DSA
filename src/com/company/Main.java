e79199950a300acd5a074ee6ae8fc9a052f64bb9
gauavdeshmukh1703@gmail.com <gauravdeshmukh1703@gmail.com>
2022-03-11 20:44:35 +0530

    Functions in JAva

---
+++ src/com/company/Main.java
@@ -0,0 +1,108 @@
+
+/*package com.company;
+
+public class Main{
+
+    public static void main(String args[]){
+
+        int n=7;
+        int r=4;
+
+        int nf=1;
+        for(int i=1;i<=n;i++){
+            nf=nf*i;
+        }
+
+        int rf=1;
+        for(int i=1;i<=r;i++){
+            rf=rf*i;
+        }
+
+        int nmrf=1;
+        for(int i=1;i<=(n-r);i++){
+            nmrf=nmrf*i;
+        }
+
+        int ans=nf/(rf*nmrf);
+        System.out.println(ans);
+    }
+}*/
+
+//-----------------------------------------------------------------------------------------
+
+/*package com.company;
+
+public class Main{
+
+    public static int factorial(int x){
+
+        int xf=1;
+        for(int i=1;i<=x;i++){
+
+            xf=xf*i;
+        }
+
+        return xf;
+    }
+
+    public static void main(String args[]){
+
+        int n=5;
+        int r=3;
+
+        int ans=factorial(n)/(factorial(r)*factorial(n-r));
+
+        System.out.println(ans);
+    }
+}*/
+
+//---------------------------------------------------------------------------------------
+
+/*package com.company;
+
+import java.util.*;
+
+public class Main{
+
+    public static void main(String args[]){
+
+        Scanner scn=new Scanner(System.in);
+        long n=scn.nextLong();  // n=number
+        int d=scn.nextInt();   // d=digit to be calculated in n
+
+        int count=0;
+        while(n!=0){
+
+            long remainder=n%10;
+            if(remainder==d){
+
+                count++;
+            }
+
+            n=n/10;
+        }
+        System.out.println(count);
+
+    }
+}*/
+
+//------------------------------------------------------------------------------------
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
