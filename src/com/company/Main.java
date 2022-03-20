594aaa52aa746d1e30e049f351ddce7872ef3674
gauavdeshmukh1703@gmail.com <gauravdeshmukh1703@gmail.com>
2022-03-20 17:57:07 +0530

    difference between max and min in array

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -413,7 +413,9 @@ public class Main{
 
 //---------------------------------------------------------------------------
 
-package com.company;
+// ANY BASE MULTIPLICATION
+
+/*package com.company;
 
 import java.util.*;
 
@@ -487,7 +489,45 @@ public class Main{
         System.out.println(abm);
     }
 
+}*/
+
+//----------------------------------------------------------------------------------------------
+
+package com.company;
+
+import java.util.*;
+
+public class Main{
+
+    public static void main(String args[]){
+
+        Scanner scn=new Scanner(System.in);
+        int n=scn.nextInt();
+        int arr[]=new int[n];
+
+        for(int i=0;i<n;i++){
+            arr[i]=scn.nextInt();
+        }
+
+        int max=arr[0];
+        for(int i=0;i<n;i++){
+            if(arr[i]>max){
+                max=arr[i];
+            }
+        }
+
+        int min=arr[0];
+        for(int i=0;i<n;i++){
+            if(arr[i]<min){
+                min=arr[i];
+            }
+        }
+
+        int diff=max-min;
+        System.out.println(diff);
+    }
 }
 
+//----------------------------------------------------------------------------------
 
 
