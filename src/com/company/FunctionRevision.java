baa8ec1c4af4c8809edcbcf693356d61ff50e2d3
gauavdeshmukh1703@gmail.com <gauravdeshmukh1703@gmail.com>
2022-04-22 14:49:18 +0530

    factorial question done

--- src/com/company/FunctionRevision.java
+++ src/com/company/FunctionRevision.java
@@ -1,2 +1,39 @@
-package com.company;public class FunctionRevision {
-}
+
+// CALCULATION OF FACTORIAL
+
+/*package com.company;
+
+import java.util.*;
+
+public class FunctionRevision {
+
+    public static int factorial(int x){
+
+        int fact=1;
+        for(int i=x;i>=1;i--){
+
+            fact=fact*i;
+        }
+        return fact;
+    }
+
+    public static void main(String args[]){
+
+        Scanner scn=new Scanner(System.in);
+        int n=scn.nextInt();
+        int r=scn.nextInt();
+
+        int nf=factorial(n);
+        int nmrf=factorial(n-r);
+        int rf=factorial(r);
+
+        int ans=nf/(nmrf*rf);
+        System.out.println(ans);
+    }
+
+}*/
+
+//-------------------------------------------------------------------------------------------------
+
+
+
